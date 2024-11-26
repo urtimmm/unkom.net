5c8c0fcfb6bbc4e9da4dbfc8104c845b489cf0c0
Timur Golochev <urtimchevgo71apmf@outlook.com>
2024-11-26 12:58:45 +0300

    normal saving

--- js/burg-script.js
+++ js/burg-script.js
@@ -1,23 +1,13 @@
 
-  document.addEventListener("DOMContentLoaded", () => {
-    const menuButton = document.querySelector(".checkbox");
-    const menuOverlay = document.querySelector(".menu-overlay");
-    const closeButton = document.querySelector(".close-button");
+document.addEventListener("DOMContentLoaded", () => {
+  const menuButton = document.querySelector(".checkbox");
+  const menuOverlay = document.querySelector(".menu-overlay");
 
-    // Открыть меню
-    menuButton.addEventListener("click", () => {
-      menuOverlay.classList.add("open");
-    });
-
-    // Закрыть меню
-    closeButton.addEventListener("click", () => {
-      menuOverlay.classList.remove("open");
-    });
-
-    // Закрыть меню при клике вне контента
-    menuOverlay.addEventListener("click", (event) => {
-      if (!event.target.closest(".menu-content")) {
-        menuOverlay.classList.remove("open");
-      }
-    });
+  // Переключение меню
+  menuButton.addEventListener("click", () => {
+    menuOverlay.classList.toggle("open"); // Добавить/удалить класс "open"
   });
+
+
+
+});
